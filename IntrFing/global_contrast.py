ae040f6ce4c2edff4f0f2a398ee723c64e673e5b
Achal Dave <achalddave@live.com>
2014-04-07 02:38:28 +0000

    fix and actually use pinch
    also, N = number of pixels, not 256

--- IntrFing/global_contrast.py
+++ IntrFing/global_contrast.py
@@ -1,10 +1,12 @@
+import math
 import numpy as np
 import ImageOps
 
 import numpy.fft as fft
+from numpy import cos
 
 class GlobalContrastDetector(object):
-    def __init__(self, Np=4, c=112, decision=100):
+    def __init__(self, Np=4, c=112, decision=0.2):
         self.Np = Np
         self.c = c
         self.decision = decision
@@ -20,10 +22,10 @@ class GlobalContrastDetector(object):
         values = np.arange(256)
 
         first = np.where(values <= self.Np)
-        p[first] = .5 - .5 * cos(math.pi * first[0] / float(N.p))
+        p[first] = .5 - .5 * cos(math.pi * first[0] / float(self.Np))
 
-        second = np.where(values >= 255 - N.p)
-        p[second] = .5 + .5 * cos(math.pi * (second[0] - 255 + N.p) / N.p)
+        second = np.where(values >= 255 - self.Np)
+        p[second] = .5 + .5 * cos(math.pi * (second[0] - 255 + self.Np) / self.Np)
 
         return p
 
@@ -41,10 +43,11 @@ class GlobalContrastDetector(object):
         img = ImageOps.grayscale(img)
         histogram = np.array(img.histogram())
         hist_fft = fft.fft(img.histogram())
+        pinched_fft = hist_fft * self.pinch()
 
         beta = np.zeros(256)
         beta[self.c:128] = 1
-        return np.sum(np.abs(beta * hist_fft)) / 256.0
+        return np.sum(np.abs(beta * pinched_fft)) / np.sum(histogram)
 
     def classify(self, img):
         return (self.fft_energy(img) > self.decision)
