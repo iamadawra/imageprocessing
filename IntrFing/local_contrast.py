7843763d52debe9d4baff66de3b44f74398c045f
iamadawra <aayushgopaldawra@gmail.com>
2014-04-15 19:52:42 -0700

    Fixed bugs in local contrast detection. Does not seem to be robust, but no major bugs apparent

--- IntrFing/local_contrast.py
+++ IntrFing/local_contrast.py
@@ -27,45 +27,48 @@ class LocalContrastDetector(object):
         """
         return Image.new('RGB', (size, size), "black")
 
-    def checkLocalContrast(self, img, boxSize=200):
+    def checkLocalContrast(self, img, boxSize=50):
         """
         Breaks the image into boxSize x boxSize sized chunks and applies the
         Global Contrast Enhancement algorithm to check for Local Contrast Enhancement.
         """
         #create a copy of the image
         img = img.copy()
-        imgWidth = img.size[0]
-        imgHeight = img.size[1]
+        imgWidth, imgHeight = img.size
         currX = 0
         currY = 0
         flag = True
         while(flag):
-            print("currX: " + str(currX))
+            print("currX+boxSize: " + str(currX+boxSize))
             print("currY: " + str(currY))
             if currX+boxSize <= imgWidth and currY+boxSize <= imgHeight:
                 print("Checkpoint 1")
                 #Case where image is being chopped up into perfect squares
-                box = (currX, currY, boxSize, boxSize)
+                box = (currX, currY, currX + boxSize, currY + boxSize)
+                print("BOX: " + str(box))
                 region = img.crop(box)
+                print("Region: " + str(region.size))
                 #compare fft_energy of enhanced image with original image over here
                 img.paste(self.checkLocalContrastHelper(region), box)
 
                 #Reset currX and currY
                 currX = currX + boxSize
+                continue
             if currX+boxSize <= imgWidth and currY+boxSize > imgHeight:
                 print("Checkpoint 2")
                 #Case where there is enough width but not enough height
-                box = (currX, currY, boxSize, (imgHeight - currY))
+                box = (currX, currY, currX + boxSize, imgHeight)
                 region = img.crop(box)
                 #compare fft_energy of enhanced image with original image over here
                 img.paste(self.checkLocalContrastHelper(region), box)
 
                 #Reset currX and currY
                 currX = currX + boxSize
+                continue
             if currX+boxSize > imgWidth and currY+boxSize <= imgHeight:
                 print("Checkpoint 3")
                 #Case where there is enough height but not enough width
-                box = (currX, currY, (imgWidth - currX), boxSize)
+                box = (currX, currY, imgWidth, currY + boxSize)
                 region = img.crop(box)
                 #compare fft_energy of enhanced image with original image over here
                 img.paste(self.checkLocalContrastHelper(region), box)
@@ -73,10 +76,11 @@ class LocalContrastDetector(object):
                 #Reset currX and currY
                 currX = 0
                 currY = currY + boxSize
+                continue
             else:
                 print("Checkpoint 4")
                 #Case where there is not enough width and not enough height
-                box = (currX, currY, (imgWidth - currX), (imgHeight - currY))
+                box = (currX, currY, imgWidth, imgHeight)
                 region = img.crop(box)
                 #compare fft_energy of enhanced image with original image over here
                 img.paste(self.checkLocalContrastHelper(region), box)
@@ -87,7 +91,7 @@ class LocalContrastDetector(object):
         #display the final Image        
         img.show()
 
-    def checkLocalContrastHelper(self, img, thresholdValue=0.5):
+    def checkLocalContrastHelper(self, img, thresholdValue=1.5):
         """
         Function to compare the contrast of a subpart of a given image.
         """
@@ -96,17 +100,20 @@ class LocalContrastDetector(object):
         contrastedImg = ImageOps.autocontrast(img)
         originalFFT = self.gcd.fft_energy(originalImg)
         contrastedFFT = self.gcd.fft_energy(contrastedImg)
-
+        print("Threshold: " + str(thresholdValue))
+        print("Value: " + str(math.fabs(originalFFT-contrastedFFT)))
         if (math.fabs(originalFFT-contrastedFFT) >= thresholdValue):
-            return img
-        return self.blackBorderImage(img)
+            print("I'm here!")
+            return self.blackBorderImage(img)
+        return img
 
-    def blackBorderImage(self, img, borderWidth=2):
-        imgWidth = img.size[0]
-        imgHeight = img.size[1]
-        box = (borderWidth, borderWidth, (imgWidth - borderWidth), (imgHeight - borderWidth))
+    def blackBorderImage(self, img, borderWidth=1):
+        imageWidth, imageHeight = img.size
+        print("imageWidth: "+ str(imageWidth))
+        print("imageHeight: " + str(imageHeight))
+        box = (borderWidth, borderWidth, (imageWidth - (borderWidth*2)), (imageHeight - (borderWidth*2)))
+        print("box: " + str(box))
         croppedImage = img.crop(box)
-        new_im = Image.new("RGB", (imgWidth, imgHeight))   ## luckily, this is already black!
+        new_im = Image.new("RGB", (imageWidth, imageHeight))   ## luckily, this is already black!
         new_im.paste(croppedImage, box)
         return new_im
-
